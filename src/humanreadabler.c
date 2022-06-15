6f36e63354cea14add05bc3619577e1767b593b6
M0x1m <max9005574181@mail.ru>
2022-06-15 18:22:59 +0300

    Add translate switchers

--- src/humanreadabler.c
+++ src/humanreadabler.c
@@ -5,22 +5,31 @@
 #include <sys/ioctl.h>
 #include <poll.h>
 
+#define KILO 1 << 0
+#define MEGA 1 << 1
+#define GIGA 1 << 2
+
+char SHORTS = (KILO | MEGA | GIGA);
+
 #define streq(s1, s2) strcmp(s1, s2) == 0
 #define isdigit(c) (c > 47 && c < 58)
 
 void usage(FILE* stream, const char* arg0){
-  fprintf(stream, "Usage: <cmd> | %s\n", arg0);
+  fprintf(stream, "Usage: <cmd> | %s -[kmg] [args]\n", arg0);
   fprintf(stream, "  Arguments:\n");
+  fprintf(stream, "    --kilo | -k   Disables kilos translates in strings\n");
+  fprintf(stream, "    --mega | -m   Disables megas translates in strings\n");
+  fprintf(stream, "    --giga | -g   Disables gigas translates in strings\n");
   fprintf(stream, "    --help | -h    Prints this message in stdout.\n");
 }
 
 static char* hnums(unsigned long num){
   static char buf[23];
-  if(num >= 1 << 10 && num <= 1 << 20){
+  if(num >= 1 << 10 && (num <= 1 << 20 || (SHORTS & (MEGA | GIGA)) != 0) && (SHORTS & KILO) != 0){
     sprintf(buf, "%ldK", num/(1 << 10));
-  }else if(num >= 1 << 20 && num <= 1 << 30){
+  }else if(num >= 1 << 20 && (num <= 1 << 30 || (SHORTS & (GIGA)) != 0) && (SHORTS & MEGA) != 0){
     sprintf(buf, "%ldM", num/(1 << 20));
-  }else if(num >= 1 << 30/* && num <= 1LLU << 40LLU*/){
+  }else if(num >= 1 << 30/* && num <= 1LLU << 40LLU*/ && (SHORTS & GIGA) != 0){
     sprintf(buf, "%ldG", num/(1 << 30));
   }else{
     sprintf(buf, "%ld", num);
@@ -50,6 +59,7 @@ void* transl_nums(char* txt){
     if(d){
       for(; isdigit(*txt2); ++txt2);
       char num[(txt2 - txt) + 1];
+      num[txt2 - txt] = 0;
       memcpy(num, txt, txt2 - txt);
       const char* n = hnums(atol(num));
       len += strlen(n);
@@ -67,16 +77,38 @@ void* transl_nums(char* txt){
   return rret;
 }
 
+int parsearg(const char * arg, const char* fmt, char* target){
+  if(*arg != '-') return -1;
+  if(*(arg+1) == '-') return -1;
+  ++arg;
+  if(strlen(arg) > strlen(fmt)) return -1;
+  for(int i = 0; i < strlen(arg); ++i){
+    for(int j = 0; j < strlen(fmt); ++j){
+      if(arg[i] == fmt[j]){
+        *target &= ~(1 << j);
+      }
+    }
+  }
+  return 0;
+}
+
 int main(int argc, char** argv){
   for (int i = 1; i < argc; ++i){
     if (streq(argv[i], "-h") || streq(argv[i], "--help")){
       usage(stdout, argv[0]);
       exit(0);
+    }else if (streq(argv[i], "--kilo")){
+      SHORTS &= ~KILO;
+    }else if (streq(argv[i], "--mega")){
+      SHORTS &= ~MEGA;
+    }else if (streq(argv[i], "--giga")){
+      SHORTS &= ~GIGA;
     }else{
+      if (parsearg(argv[i], "kmg", &SHORTS) == 0) continue;
       usage(stderr, argv[0]);
       exit(1);
     }
-  }
+  }  
   struct pollfd fd = {
     .fd = 0,
     .events = POLLIN | POLLRDHUP
